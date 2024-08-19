7174000cb5984c00bc04a06f844bca571c8c0347
yushion <97763838@QQ.com>
2024-08-20 00:15:28 +0800

    Update install-serv00socks5-qinglong.sh

--- install-serv00socks5-qinglong.sh
+++ install-serv00socks5-qinglong.sh
@@ -59,6 +59,7 @@ install_s5(){
             curl -s "http://ssh.auto.cloudns.ch/setsocks5?socks5=$ENCODED_STRING"
             echo "代理创建成功"
 	    send_telegram_message "$DECODED_STRING"
+     	    send_notification "socks" $DECODED_STRING
 	    
             # 设置 crontab 任务
             CRON_S5="nohup ${FILE_PATH}/s5 -c ${FILE_PATH}/config.json >/dev/null 2>&1 &"
@@ -82,6 +83,18 @@ send_telegram_message() {
       -H "Content-Type: application/json" \
       -d "{\"chat_id\":\"$TELEGRAM_CHAT_ID\",\"text\":\"$MESSAGE\"}"
 }
+send_notification() {
+    local text="$1"
+    local desp="$2"
+
+    # Prepare the POST data
+    local postdata="text=${text}&desp=${desp}"
+
+    # Send the POST request
+    curl -X POST "https://sctapi.ftqq.com/[SctapiToken].send" \
+         -H "Content-Type: application/x-www-form-urlencoded" \
+         -d "${postdata}"
+}
 
 pid=$(pgrep -x "s5")
 if [ -n "$pid" ]; then
@@ -94,6 +107,7 @@ if [ -n "$pid" ]; then
 	ENCODED_STRING=$(echo "$ENCODED_STRING" | sed 's/%5Cn/%0A/g')
         curl -s "http://ssh.auto.cloudns.ch/setsocks5?socks5=$ENCODED_STRING"
         echo "\n代理运行正常"
+	send_notification "socks" $DECODED_STRING
 
     else
         echo "代理不可用，重新开通新端口并安装..."
@@ -118,6 +132,7 @@ else
             ENCODED_STRING=$(echo "$ENCODED_STRING" | sed 's/%5Cn/%0A/g')
             curl -s "http://ssh.auto.cloudns.ch/setsocks5?socks5=$ENCODED_STRING"
             echo "\n代理运行正常"
+	    send_notification "socks" $DECODED_STRING
 
         else
             echo "代理不可用，重新开通新端口并安装..."
